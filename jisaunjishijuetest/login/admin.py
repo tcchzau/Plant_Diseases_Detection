20de5818a6fe9eac3f38afd44f9c274bdeef8b07
tcchzau <106466103+tcchzau@users.noreply.github.com>
2024-06-13 21:01:23 +0800

    植物病害检测demo

---
+++ jisaunjishijuetest/login/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from login.models import SiteUser
+# ,ConfirmString
+
+# class SiteUserAdmin(admin.ModelAdmin):
+#     list_display = ['name','gender','email']
+#     list_display_links = ['name']
+#     list_filter = ['gender','create_time']
+
+# admin.site.register(SiteUser,SiteUserAdmin)
+# admin.site.register(ConfirmString)
+
